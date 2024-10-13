af3d733a1a266f46fa8ae9815137f095ed8a34f4
AnalyticsAskew <askewanalysis@gmail.com>
2024-10-13 01:31:51 -0400

    Subclass & inheritance concepts

--- Python_BC_60_day/Todo_input_basics-review_scrap/main.py
+++ Python_BC_60_day/Todo_input_basics-review_scrap/main.py
@@ -1,6 +1,22 @@
-todo_dict = {}
+class Character:
 
-Running = True
-while Running == 1:
-    selection = input("Todo list! 'add' 'remove' or 'show' tasks?")
-    if selection == 'add':
+    def __init__(self, x, y, num_lives):
+        self.x = x
+        self.y = y
+        self.num_lives = num_lives
+
+
+class Player(Character):
+    INITIAL_X = 0
+    INITIAL_Y = 0
+    INITIAL_NUM_LIVES = 10
+
+    def __init__(self, score=0):
+        Character.__init__(self, Player.INITIAL_X, Player.INITIAL_Y, Player.INITIAL_NUM_LIVES)
+
+
+class Enemy(Character):
+
+    def __init__(self, x=15, y=15, num_lives=8, is_poisonous=False):
+        Character.__init__(self, x, y, num_lives)
+        self.is_poisonous = is_poisonous
